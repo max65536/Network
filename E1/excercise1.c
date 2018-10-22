919874abbf37132d7fd7a32d3f53ee16da8f9e34
maxlqs <maxinteger65536@gmail.com>
2018-10-23 04:51:54 +0800

    latest exerciseC

--- E1/excercise1.c
+++ E1/excercise1.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : test.c
- Author      : 
+ Author      :
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
@@ -15,23 +15,22 @@ struct route{
 	char descrp[25];
 };
 typedef struct route Route;
-struct route longroutes[10];
-struct route* routePtr;
+Route longroutes[10];
+Route route1;
+Route* routePtr;
 
 
 
 
 int main(void) {
-	Route route1;
 	printf("please input routeID\n");
     scanf("%d", &route1.routeID);
-	printf("please input descrp\n");
+	printf("please input descrption\n");
     scanf("%s", &route1.descrp);
-    printf("routeID is %d\n",route1.routeID);
-    printf("descrp is %s\n",route1.descrp);
     longroutes[3] = route1;
     routePtr = longroutes;
-    printf("longroutes[3] is %s\n", (routePtr+3)->descrp);
+    printf("routeID of longroutes[3] is %d\n", (routePtr+3)->routeID);
+    printf("descrp of longroutes[3] is %s\n", (routePtr+3)->descrp);
 
 
 
--- E1/excercise2.c
+++ E1/excercise2.c
@@ -9,10 +9,13 @@ int main(void) {
     tmpPtr = &taxrate;
     printf("the value of tempPtr is %.1f\n", *tmpPtr);
     discountrate = *tmpPtr;
+    printf("discountrate is %.1f\n", discountrate);
     printf("taxrate addr is %p\n", &taxrate);
-    printf("tmpPtr addr is %p\n", tmpPtr);
+    printf("tmpPtr stored addr is %p\n", tmpPtr);
     strcpy(buyer,"network programming");
     strcpy(seller,buyer);
+    printf("seller and buyer are ");
+    if (strcmp(seller,buyer)) printf("different\n"); else printf("the same.\n");
     printf("buyer is %s\n", buyer);
     printf("seller is %s\n", seller);
     strcat(buyer,seller);
