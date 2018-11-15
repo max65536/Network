84417efac4a9c7dc622ac80dac2bb9cc270ba01f
linqiucen <44300338+linqiucen@users.noreply.github.com>
2018-11-15 21:53:22 +0100

    without analysis of the packet

---
+++ select/packet.c
@@ -0,0 +1,58 @@
+/*
+ ============================================================================
+ Name        : Server.c
+ Author      : np
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netpacket/packet.h>
+#include <net/ethernet.h>
+#include <linux/if_ether.h>
+
+#define MAX_MSG_SIZE 1024
+
+int main(void) {
+    char recvbuf[MAX_MSG_SIZE];
+    char* ether;
+    int packet_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    int i;
+    struct sockaddr_ll LLAddr;
+    int addrlen = sizeof (LLAddr);
+    struct packet_mreq Mreq;
+    Mreq.mr_type = PACKET_MR_PROMISC;
+    setsockopt(packet_socket, SOL_PACKET, PACKET_ADD_MEMBERSHIP, &Mreq, sizeof(Mreq));
+
+    while(1){
+        memset(recvbuf,0,MAX_MSG_SIZE);
+        recvfrom(packet_socket, recvbuf, sizeof(recvbuf), 0, (struct sockaddr*) &LLAddr,&addrlen);
+        //ether = recvbuf;
+        for(i=0; i<MAX_MSG_SIZE;i++){
+            if(i==0){
+                printf("MSG:");
+            }
+            printf(" %x ", recvbuf[i]);
+           if(recvbuf[i]==0 && recvbuf[i+1]==0 && recvbuf[i+1]==0){
+               break; // under this circumstance, we think the recv data stops here
+           }
+        }
+
+        printf("\n\n");
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
